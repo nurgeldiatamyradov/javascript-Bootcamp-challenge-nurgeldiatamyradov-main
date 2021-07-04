75d8c762c85f6500c7f24569b8539eb2821d8362
nurgeldi atamyradov <67277491+nurgeldiatamyradov@users.noreply.github.com>
2021-07-04 19:53:57 +0300

    commıt

---
+++ development/assets/script/main.js
@@ -0,0 +1,609 @@
+let data = [
+	{
+		"id": 1,
+		"name": "Ahmed Bustani",
+		"assistant": true,
+		"group": "Orange"
+	},
+	{
+		"id": 2,
+		"name": "Baris Tunar",
+		"assistant": true,
+		"group": "Crimson"
+	},
+	{
+		"id": 3,
+		"name": "Burak Caglar",
+		"assistant": true,
+		"group": "Khaki"
+	},
+	{
+		"id": 4,
+		"name": "ah. fawad",
+		"assistant": true,
+		"group": "Pink"
+	},
+	{
+		"id": 5,
+		"name": "Furkan KILIÇER",
+		"assistant": true,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 6,
+		"name": "Alperen Aslan",
+		"assistant": true,
+		"group": "Turquoise"
+	},
+	{
+		"id": 7,
+		"name": "joudi bitar",
+		"assistant": true,
+		"group": "Yellow"
+	},
+	{
+		"id": 8,
+		"name": "Kübra Özdem",
+		"assistant": true,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 9,
+		"name": "Aycan",
+		"assistant": true,
+		"group": "Fuscia"
+	},
+	{
+		"id": 10,
+		"name": "Aykut Şahin",
+		"assistant": true,
+		"group": "Puce"
+	},
+	{
+		"id": 11,
+		"name": "S. Zahit ACAR",
+		"assistant": true,
+		"group": "Maroon"
+	},
+	{
+		"id": 12,
+		"name": "Tugay Türk",
+		"assistant": true,
+		"group": "Purple"
+	},
+	{
+		"id": 13,
+		"name": "Abdurrahman Seyidoğlu",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 14,
+		"name": "Ahmed Tabşo",
+		"assistant": false,
+		"group": "Crimson"
+	},
+	{
+		"id": 15,
+		"name": "Alaa Alaff",
+		"assistant": false,
+		"group": "Fuscia"
+	},
+	{
+		"id": 16,
+		"name": "ASSKOUR Imane",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 17,
+		"name": "Aydoğan Gökberk",
+		"assistant": false,
+		"group": "Khaki"
+	},
+	{
+		"id": 18,
+		"name": "BAHADDİN ALMUHAMMED",
+		"assistant": false,
+		"group": "Khaki"
+	},
+	{
+		"id": 19,
+		"name": "Büşra Bilgin",
+		"assistant": false,
+		"group": "Maroon"
+	},
+	{
+		"id": 20,
+		"name": "Chimnaz Vahabzade",
+		"assistant": false,
+		"group": "Orange"
+	},
+	{
+		"id": 21,
+		"name": "Çağatay Sarıoğlan",
+		"assistant": false,
+		"group": "Pink"
+	},
+	{
+		"id": 22,
+		"name": "dana alhameed",
+		"assistant": false,
+		"group": "Puce"
+	},
+	{
+		"id": 23,
+		"name": "Delfin Öztürk",
+		"assistant": false,
+		"group": "Purple"
+	},
+	{
+		"id": 24,
+		"name": "Ebubekir Karataş",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 25,
+		"name": "Ela Yağmur ",
+		"assistant": false,
+		"group": "Turquoise"
+	},
+	{
+		"id": 26,
+		"name": "ELNAZ HAMEDIBAZAZ",
+		"assistant": false,
+		"group": "Pink"
+	},
+	{
+		"id": 27,
+		"name": "Fatma Şehitoğlu",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 28,
+		"name": "Feras Alkilani",
+		"assistant": false,
+		"group": "Fuscia"
+	},
+	{
+		"id": 29,
+		"name": "Gizem Nur Korkmaz",
+		"assistant": false,
+		"group": "Crimson"
+	},
+	{
+		"id": 30,
+		name: "Halil İbrahim Badem",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 31,
+		"name": "Hasan ALKHALAF",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 32,
+		"name": "Hatice Sürümlü",
+		"assistant": false,
+		"group": "Purple"
+	},
+	{
+		"id": 33,
+		"name": "IMAN ALABID",
+		"assistant": false,
+		"group": "Maroon"
+	},
+	{
+		"id": 34,
+		"name": "Işıl Özkaynak",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 35,
+		"name": "İbrahim Halil DÖŞ",
+		"assistant": false,
+		"group": "Crimson"
+	},
+	{
+		"id": 36,
+		"name": "İlkay Çitak",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 37,
+		"name": "Kenan Mirzeyev",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 38,
+		"name": "Melisa Gürdal",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 39,
+		"name": "Merve Şahin",
+		"assistant": false,
+		"group": "Orange"
+	},
+	{
+		"id": 40,
+		"name": "MHD NABIL ARAR",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 41,
+		"name": "Muhammet BİLGİN",
+		"assistant": false,
+		"group": "Orange"
+	},
+	{
+		"id": 42,
+		"name": "Nurgeldi Atamyradov",
+		"assistant": false,
+		"group": "Khaki"
+	},
+	{
+		"id": 43,
+		"name": "Ozan Ali TÜRKYILMAZ",
+		"assistant": false,
+		"group": "Pink"
+	},
+	{
+		"id": 44,
+		"name": "Özge Yurdusev",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 45,
+		"name": "Parahat Nepesov",
+		"assistant": false,
+		"group": "Puce"
+	},
+	{
+		"id": 46,
+		"name": "Rined haj osman",
+		"assistant": false,
+		"group": "Turquoise"
+	},
+	{
+		"id": 47,
+		"name": "Serkan Çelik",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 48,
+		"name": "Sinem Nurioğlu",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 49,
+		"name": "Şifa Hasani",
+		"assistant": false,
+		"group": "Turquoise"
+	},
+	{
+		"id": 50,
+		"name": "Yasemin Paşaoğlu",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id":             51,
+		"name":           "Yusuf sabbagh",
+		"assistant":      false,
+		"group":          "Crimson"
+	},
+	{
+		"id": 52,
+		"name": "Nurgeldi Atamyradov",
+		"assistant": false,
+		"group": "Yellow"
+	}
+];
+
+const nonduplicatdeData = data.reduce((acc, current) => {
+	const x = acc.find(item => item.name === current.name);
+	if (!x) {
+	  return acc.concat([current]);
+	} else {
+	  return acc;
+	}
+  }, []);
+
+  console.log(nonduplicatdeData);
+
+
+
+//Yellow Group
+let yellowGroup = nonduplicatdeData.filter(item => item.group == "Yellow");
+let yellowAssistant = yellowGroup.filter(item => item.assistant == true);
+let yellowStudent = yellowGroup.filter(item => item.assistant == false);
+console.log(yellowGroup);
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  }
+  
+  document.querySelector("[yellow-group]").innerHTML = `
+	<ol>
+	  ${createListItems(yellowGroup)}
+	</ol>
+  `;
+
+  createListItems(yellowGroup);
+
+
+//Crimson Group
+let crimsonGroup = nonduplicatdeData.filter(item => item.group == "Crimson");
+let crimsonAssistant = crimsonGroup.filter(item => item.assistant == true);
+let crimsonStudents = crimsonGroup.filter(item => item.assistant == false);
+console.log(crimsonGroup);
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  }
+  
+  document.querySelector("[crimson-group]").innerHTML = `
+	<ol>
+	  ${createListItems(crimsonGroup)}
+	</ol>
+  `;
+
+  createListItems(crimsonGroup);
+
+
+//Turquoise Group
+let turquoiseGroup = nonduplicatdeData.filter(item => item.group == "Turquoise");
+let turquoiseAssistan = turquoiseGroup.filter(item => item.assistant == true);
+let turquoiseStudents = turquoiseGroup.filter(item => item.assistant == false);
+console.log(turquoiseGroup);
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  }
+  
+  document.querySelector("[turquoise-group]").innerHTML = `
+	<ol>
+	  ${createListItems(turquoiseGroup)}
+	</ol>
+  `;
+
+  createListItems(turquoiseGroup);
+
+
+//Aquamarine Group
+let aquamarineGroup = nonduplicatdeData.filter(item => item.group == "Aquamarine");
+let aquamarineAssistan = aquamarineGroup.filter(item => item.assistant == true);
+let aquamarineStudents = aquamarineGroup.filter(item => item.assistant == false);
+console.log(aquamarineGroup);
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  }
+  
+  document.querySelector("[aquamarine-group]").innerHTML = `
+	<ol>
+	  ${createListItems(aquamarineGroup)}
+	</ol>
+  `;
+
+  createListItems(aquamarineGroup);
+
+
+//Puce Group
+let puceGroup = nonduplicatdeData.filter(item => item.group == "Puce");
+let puceAssistan = puceGroup.filter(item => item.assistant == true);
+let puceStudents = puceGroup.filter(item => item.assistant == false);
+console.log(puceGroup);
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  }
+  
+  document.querySelector("[puce-group]").innerHTML = `
+	<ol>
+	  ${createListItems(puceGroup)}
+	</ol>
+  `;
+
+  createListItems(puceGroup);
+
+
+//Goldenrod Group
+let goldenrodGroup = nonduplicatdeData.filter(item => item.group == "Goldenrod");
+let goldenrodAssistan = goldenrodGroup.filter(item => item.assistant == true)
+let goldenrodStudents = goldenrodGroup.filter(item => item.assistant == false)
+console.log(goldenrodGroup);
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  }
+  
+  document.querySelector("[goldenrod-group]").innerHTML = `
+	<ol>
+	  ${createListItems(goldenrodGroup)}
+	</ol>
+  `;
+
+  createListItems(goldenrodGroup);
+
+//Orange Group
+let orangeGroup = nonduplicatdeData.filter(item => item.group == "Orange");
+let orangeAssistan = orangeGroup.filter(item => item.assistant == true);
+let orangeStudents = orangeGroup.filter(item => item.assistant == false);
+console.log(orangeGroup);
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  }
+  
+  document.querySelector("[orange-group]").innerHTML = `
+	<ol>
+	  ${createListItems(orangeGroup)}
+	</ol>
+  `;
+
+  createListItems(orangeGroup);
+
+//Maroon Group
+let maroonGroup = nonduplicatdeData.filter(item => item.group == "Maroon");
+let maroonAssistan = maroonGroup.filter(item => item.assistant == true);
+let maroonStudents = maroonGroup.filter(item => item.assistant == false);
+console.log(maroonGroup);
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  }
+  
+  document.querySelector("[maroon-group]").innerHTML = `
+	<ol>
+	  ${createListItems(maroonGroup)}
+	</ol>
+  `;
+
+  createListItems(maroonGroup);
+
+
+//Pink Group
+let pinkGroup = nonduplicatdeData.filter(item => item.group == "Pink");
+let pinkAssistan = pinkGroup.filter(item => item.assistant == true);
+let pinkStudents = pinkGroup.filter(item => item.assistant == false);
+console.log(pinkGroup);
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  }
+  
+  document.querySelector("[pink-group]").innerHTML = `
+	<ol>
+	  ${createListItems(pinkGroup)}
+	</ol>
+  `;
+
+  createListItems(pinkGroup);
+
+
+
+
+//Fuscia Group
+let fusciaGroup = nonduplicatdeData.filter(item => item.group == "Fuscia");
+let fusciaAssistan = fusciaGroup.filter(item => item.assistant == true);
+let fusciaStudents = fusciaGroup.filter(item => item.assistant == false);
+console.log(fusciaGroup);
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  }
+  
+  document.querySelector("[fuscia-group]").innerHTML = `
+	<ol>
+	  ${createListItems(fusciaGroup)}
+	</ol>
+  `;
+
+  createListItems(fusciaGroup);
+
+//Purple Group
+let purpleGroup = nonduplicatdeData.filter(item => item.group == "Purple");
+let purpleAssistan = purpleGroup.filter(item => item.assistant == true);
+let purpleStudents = purpleGroup.filter(item => item.assistant == false);
+console.log(purpleGroup);
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  };
+  
+  document.querySelector("[purple-group]").innerHTML = `
+	<ol>
+	  ${createListItems(purpleGroup)}
+	</ol>
+  `;
+
+  createListItems(purpleGroup);
+
+
+
+
+  //Khaki Group 
+let khakiGroup = nonduplicatdeData.filter(item => item.group == "Khaki");
+let khakiAssistan = khakiGroup.filter(item => item.assistant == true);
+let khakiStudents = khakiGroup.filter(item => item.assistant == false);
+console.log(khakiGroup);
+
+
+let khakiName = khakiGroup.forEach(function(khakis) {
+	console.log(khakis.name)
+});
+
+
+function createListItems( arr ) {
+	let items = '';
+	for ( let i = 0; i < arr.length; i++ ) {
+	  items += `<li>${ arr[i] }</li>`;
+	}
+	return items;
+  };
+  
+  document.querySelector("[khaki-group]").innerHTML = `
+	<ol>
+	  ${createListItems(khakiGroup)}
+	</ol>
+  `;
+
+  createListItems(khakiGroup);
